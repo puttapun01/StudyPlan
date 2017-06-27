a84b87da3fb0fb3893ef0f06939a4b3d6b2877cd
Puttapan Chompan <puttapun01@gmail.com>
2017-06-28 00:12:22 +0700

    Prepare

--- app/src/main/java/th/ac/rmutt/comsci/studyplan/Activity/AboutActivity.java
+++ app/src/main/java/th/ac/rmutt/comsci/studyplan/Activity/AboutActivity.java
@@ -1,13 +1,15 @@
-package th.ac.rmutt.comsci.studyplan;
+package th.ac.rmutt.comsci.studyplan.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import th.ac.rmutt.comsci.studyplan.R;
+
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
     }
 }
